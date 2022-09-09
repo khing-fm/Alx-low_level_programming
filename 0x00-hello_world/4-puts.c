4c05748f2024eebaab55bbbcd63c72b6418588c8
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-08 20:13:19 -0700

    This is a low level programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+
 /**
 * main - prints the phrase "Progreamming is like building a multilingual puzzle"
 *
 * Return: 0 if exited properly, non-zero otherwise
 */
+
 int main(void)
+  
 {
+  
   puts("\"Programming is like building a multilingual puzzle");
+  
   return (0);
+  
 }

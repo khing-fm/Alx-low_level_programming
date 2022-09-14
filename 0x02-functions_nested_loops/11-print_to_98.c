f844fe06f6be623240094590e605fb7bd7744665
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-13 19:51:45 -0700

    Putchar level

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,37 +2,37 @@
 #include "main.h"
 
 /**
-
- * print_to_98 - prints all natural numbers from n to 98,
-
- * followed by a new line
-
- * @n: print from this number
-
- */
+* print_to_98 - prints all natural numbers from n to 98,
+* followed by a new line
+* @n: print from this number
+*/
 
 void print_to_98(int n)
-{
-int i, j;
 
-if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+f (n > 98)
 {
-if (i != 98)
-printf("%d, ", i);
-else if (i == 98)
-printf("%d\n", i);
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
 }
 }
-else if (n >= 98)
+else
+{    
+while (n <= 98)
 {
-for (j = n; j >= 98; j--)
+printf("%d", n);
+if (n != 98)
 {
-if (j != 98)
-printf("%d, ", j);
-else if (j == 98)
-printf("%d\n", j);
+printf(", ");
+}
+n++;
 }
 }
+printf("\n");
 }

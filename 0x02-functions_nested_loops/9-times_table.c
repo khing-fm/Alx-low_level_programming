0d7387032a45c7d3f3403e796ee5252d2a7c239d
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-13 19:38:27 -0700

    Putchar level

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,7 +4,7 @@
 * times_table - prints the 9 times table, starting with 0
 */
 void times_table(void)
-{  
+{
 int i, j, k;
 
 for (i = 0; i < 10; i++)
@@ -23,7 +23,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');

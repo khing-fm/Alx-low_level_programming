1e08fc22ef4634a938eca25618a4a16a67d15bd1
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-15 18:27:30 -0700

    More nested loops functions corrected

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,9 +6,9 @@
 */
 
 void print_most_numbers(void)
-
 {
 int i;
+
 for (i = 0; i < 10; i++)
 {
 if (i != 2 && i != 4)
@@ -16,5 +16,6 @@ if (i != 2 && i != 4)
 _putchar(i + '0');
 }
 }
+
 _putchar('\n');
 }

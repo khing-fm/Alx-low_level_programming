3418370f9332ae1ba23feb008204a33d3a00f825
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-15 18:30:17 -0700

    Corrected

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,6 @@
 * print_numbers - prints the numbers, from 0 to 9,
 * followed by a new line
 */
-
 void print_numbers(void)
 {
 int i;
@@ -12,5 +11,6 @@ for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
 }
+
 _putchar('\n');
 }

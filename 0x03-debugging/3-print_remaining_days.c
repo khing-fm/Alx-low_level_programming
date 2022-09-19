ec2dc451433360264fa346c09a3dc1add99642d6
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-19 03:50:01 -0700

    Fixed

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,27 +9,27 @@
 * Return: void
 */
 
-void print_remaining_days(int mm, int dd, int yyyy)
+void print_remaining_days(int month, int day, int year)
 {
-if (yyyy % 4 == 0 || ((yyyy % 100 == 0) && !(yyyy % 400 == 0)))
+if (year % 4 == 0 || ((year % 100 == 0) && !(year % 400 == 0)))
 {
-if (mm > 2 && dd >= 60)
+if (month > 2 && day >= 60)
 {
-dd++;
+day++;
 }
-printf("Day of the year: %d\n", dd);
-printf("Remaining days: %d\n", 366 - dd);
-}
-else
-{
-if (mm == 2 && dd == 60)
-{
-printf("Invalid date: %02d/%02d/%04d\n", mm, dd - 31, yyyy);
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-printf("Day of the year: %d\n", dd);
-printf("Remaining days: %d\n", 365 - dd);
+if (month == 2 && day == 60)
+{
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+}
+else
+{
+printf("Day of the year: %d\n", day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }

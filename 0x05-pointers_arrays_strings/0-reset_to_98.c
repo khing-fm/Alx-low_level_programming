522b3dd61555adfd73d38fdccb8c365acdfaf227
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-19 07:47:47 -0700

    PAS

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,21 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * reset_to_98 - updates the value of the variable
 * the pointer points to 98
 * @n: pointer to the variable to be updated
 */
+
 void reset_to_98(int n)
+
+int main(void)
 {
-*n = 98
+int n;
+
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
 }

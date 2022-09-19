28001b9fdf3188464e9a09e9da2b4a50697e9f83
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-19 02:44:09 -0700

    corrected

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,31 @@
 #include "main.h"
 
+
+
 /**
+
 * main - test for a function that gives a correct output
+
 * Return is always 0
+
 */
 
+
+
 int main(void)
+
 {
+
 int j;
 
+
+
 j = 0;
+
 positive_or_negative(j);
+
+
+
 return (0);
+
 }

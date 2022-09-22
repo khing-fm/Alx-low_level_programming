da767e08002e646c5df34b1a28010e3da3cff307
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-09-21 20:32:07 -0700

    More PAS

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,7 +15,8 @@ char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; a[j] != '\0'; j++){
+for (j = 0; a[j] != '\0'; j++)
+{
 if (s[i] == a[j])
 {
 s[i] = b[j];

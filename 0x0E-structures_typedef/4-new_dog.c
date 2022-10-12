a3debcb5dee21d18a54793e29d5b8db7f07cbd4f
Adefemi Adelugba <adefemiade3662@gmail.com>
2022-10-11 18:04:58 -0700

    Corrected

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,8 +19,7 @@ return (NULL);
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
 return (NULL);
-for (nl = 0; name[nl]; nl++)
-;
+for (nl = 0; name[nl]; nl++);
 nl++;
 dog->name = malloc(nl * sizeof(char));
 if (dog->name == NULL)
@@ -31,8 +30,7 @@ return (NULL);
 for (i = 0; i < nl; i++)
 dog->name[i] = name[i];
 dog->age = age;
-for (ol = 0; owner[ol]; ol++)
-;
+for (ol = 0; owner[ol]; ol++);
 ol++;
 dog->owner = malloc(ol * sizeof(char));
 if (dog->owner == NULL)
